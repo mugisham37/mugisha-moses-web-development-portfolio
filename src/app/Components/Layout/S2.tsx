f9b2f30c0597d9d6e0d31918447959e49fdc754c
mugisham37 <mugisham333@gmail.com>
2025-12-27 16:08:50 +0200

    Rearranged the project

---
+++ src/app/Components/Layout/S2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const s2 = () => {
+  return (
+    <div>s2</div>
+  )
+}
+
+export default s2
---
+++ src/app/Components/Layout/StickyNav.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const StickyNav = () => {
+  return (
+    <div class="framer-1xgw1dx" data-framer-name="sticky-nav"
+                style="will-change: transform, opacity, filter; z-index: 8; opacity: 0; filter: blur(10px); transform: translateX(-50%) translateY(55px) scale(0.9);">
+                <div class="framer-1fgo7cv-container" id="button"><!--$-->
+                    <div style="display:inline-flex">
+                        <div
+                            style="position:relative;display:inline-flex;background-color:#fff;border-radius:8px;gap:8px;padding:5px">
+                            <div
+                                style="position: absolute; background-color: rgb(255, 255, 255); border-radius: 4px; transition: 300ms ease-in-out; z-index: 1; left: 67px; width: 49px; height: 37px;">
+                            </div><a href="https://jarcos.work/work?ref=land-book.com"
+                                style="position:relative;padding:10px 12px;font-family:&quot;Inter Display&quot;, &quot;Inter Display Placeholder&quot;, sans-serif;font-weight:600;letter-spacing:0;font-style:normal;border-radius:8px;color:#000;text-decoration:none;z-index:10;filter:invert(1);mix-blend-mode:difference;cursor:pointer">Work</a><a
+                                href="https://jarcos.work/grid?ref=land-book.com"
+                                style="position:relative;padding:10px 12px;font-family:&quot;Inter Display&quot;, &quot;Inter Display Placeholder&quot;, sans-serif;font-weight:600;letter-spacing:0;font-style:normal;border-radius:8px;color:#000;text-decoration:none;z-index:10;filter:invert(1);mix-blend-mode:difference;cursor:pointer">Grid</a><a
+                                href="https://jarcos.work/contact?ref=land-book.com"
+                                style="position:relative;padding:10px 12px;font-family:&quot;Inter Display&quot;, &quot;Inter Display Placeholder&quot;, sans-serif;font-weight:600;letter-spacing:0;font-style:normal;border-radius:8px;color:#000;text-decoration:none;z-index:10;filter:invert(1);mix-blend-mode:difference;cursor:pointer">Inquiries</a>
+                        </div>
+                    </div><!--/$-->
+                </div>
+            </div>
+  )
+}
+
+export default StickyNav
